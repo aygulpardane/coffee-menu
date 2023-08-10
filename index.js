d7f0293fea63ee88f62634e8cd345d51d30fba07
aygulpardane <aygulparpucu@gmail.com>
2023-08-10 15:10:41 -0600

    connected two files

---
+++ index.js
@@ -0,0 +1,13 @@
+const coffeeData = require("./coffee_data.js");
+
+// 2. Print an array of all the drinks on the menu.
+
+// 3. Print an array of drinks that cost 5 and under.
+
+// 4. Print an array of drinks that are priced at an even number.
+
+// 5. Print the total if you were to order one of every drink.
+
+// 6. Print an array with all the drinks that are seasonal.
+
+// 7. Print all the seasonal drinks with the words "with imported beans" after the item name. For example: "affogato with imported beans".
