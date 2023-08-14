58e126330de3982c146557411dd93064fa6218e9
aygulpardane <aygulparpucu@gmail.com>
2023-08-14 16:07:45 -0600

    finish

--- index.js
+++ index.js
@@ -7,18 +7,28 @@ console.log(drinks);
 
 // 3. Print an array of drinks that cost 5 and under.
 
-// const drinksUnderFive = coffeeData.filter(item => item.price <= 5);
-// console.log(drinksUnderFive);
-
-const drinksUnderFive = (coffeeData) = {
-    coffeeData.price <= 5;
-    return coffeeData.name;
-};
+const drinksUnderFive = coffeeData.filter(item => item.price <= 5);
+console.log(drinksUnderFive);
 
 // 4. Print an array of drinks that are priced at an even number.
 
+const evenPricedCoffee = coffeeData.filter(item => item.price % 2 === 0);
+console.log(evenPricedCoffee);
+
 // 5. Print the total if you were to order one of every drink.
+
+const sumOfAllDrinks = coffeeData.map(item => item.price).reduce(
+    (accumulator, currentValue) => accumulator + currentValue
+);
+console.log(sumOfAllDrinks);
 
 // 6. Print an array with all the drinks that are seasonal.
 
+const seasonalDrinks = coffeeData.filter(item => item.seasonal === true);
+console.log(seasonalDrinks);
+
 // 7. Print all the seasonal drinks with the words "with imported beans" after the item name. For example: "affogato with imported beans".
+
+const seasonalDrinksName = coffeeData.filter(item => item.seasonal === true).map(item => item.name).forEach(item => console.log(item + " with imported beans"));
+console.log(seasonalDrinksName);
+
